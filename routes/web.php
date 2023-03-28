4bafdd4c95514d76fb263a2dc3b4b95b3807c24c
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-03-28 23:42:29 +0200

    create routes for partials

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,44 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('/', function () {
     return view('partials/main');
+})->name('homepage');
+
+Route::get('/characters', function () {
+    return view('partials/characters');
+})->name('characters');
+
+Route::get('/comics', function () {
+    return view('partials/comics');
+})->name('comics');
+
+Route::get('/movies', function () {
+    return view('partials/movies');
+})->name('movies');
+
+Route::get('/tv', function () {
+    return view('partials/tv');
+})->name('tv');
+
+Route::get('/games', function () {
+    return view('partials/games');
+})->name('games');
+
+Route::get('/collectibles', function () {
+    return view('partials/collectibles');
+})->name('collectibles');
+
+Route::get('/videos', function () {
+    return view('partials/videos');
+})->name('videos');
+
+Route::get('/fans', function () {
+    return view('partials/fans');
+})->name('fans');
+
+Route::get('/news', function () {
+    return view('partials/news');
+})->name('news');
+
+Route::get('/shop', function () {
+    return view('partials/shop');
 });
